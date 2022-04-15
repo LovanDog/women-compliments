a19664d3566d40ca12d75bca2d5bbfdacfe67446
lovandog <61935888+LovanDog@users.noreply.github.com>
2022-04-15 10:21:13 +0200

    Add files via upload

---
+++ compliments-women.py
@@ -0,0 +1,34 @@
+from time import sleep
+import requests
+from bs4 import BeautifulSoup
+import random
+from telethon import TelegramClient
+
+api_id = 10611839
+api_hash = 'faa6a69e15bcfe74c60e9fca06d85540'
+
+compl = []
+print('Сбор комплиментов..')
+
+html = requests.get('https://t-loves.narod.ru/komplimenty-devushke.htm')
+soup = BeautifulSoup(html.text, 'lxml')
+
+
+for link in soup.find_all('p'):
+    compl.append(link.text)
+    
+compl.pop()
+
+print(f'Собрано {len(compl)} комплиментов.')
+user = input('Введите ID получателя в Телеграм (без @): ')
+
+while True:
+    input(f'Нажмите Enter, чтобы отправить рандомный комплимент.')
+    randcompl = compl[random.randint(1,len(compl)-1)]
+    print()
+    print(randcompl)
+    
+    with TelegramClient('anon', api_id, api_hash) as client:
+        client.loop.run_until_complete(client.send_message(user, randcompl))
+        
+        
